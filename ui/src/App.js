837894e4ebb54980dff86e2a54cbc11543dbf196
tgordienko <tgordienko@larch-networks.com>
2022-11-25 18:06:20 +0200

    Add create blog feature

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import Blog from './components/Blog'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
@@ -6,103 +6,143 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
-  const [errorMessage, setErrorMessage] = useState(null)
+    const [blogs, setBlogs] = useState([])
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [user, setUser] = useState(null)
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const [url, setUrl] = useState('')
+    const [errorMessage, setErrorMessage] = useState(null)
 
-  useEffect(() => {
+    useEffect(() => {
+        blogService.getAll().then(blogs => {
+            setBlogs(blogs)
+        })
+    }, [user])
 
-  }, [])
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+        if (loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON)
+            setUser(user)
+            blogService.setToken(user.token)
+        }
+    }, [])
 
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
-    if(loggedUserJSON){
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      blogService.setToken(user.token)
-      blogService.getAll().then(blogs => {
-            setBlogs(blogs);
-          }
-      )
+    const handleLogin = async (event) => {
+        event.preventDefault()
+        try {
+            const user = await loginService.login({
+                username, password,
+            })
+
+            window.localStorage.setItem(
+                'loggedBlogAppUser', JSON.stringify(user)
+            )
+            blogService.setToken(user.token)
+            setUser(user)
+            setUsername('')
+            setPassword('')
+        } catch (exception) {
+            setErrorMessage(`Wrong credentials: ${exception.response.data.error}`)
+            setTimeout(() => {
+                setErrorMessage(null)
+            }, 5000)
+        }
     }
-  },[])
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
-    try {
-      const user = await loginService.login({
-        username, password,
-      })
-
-      window.localStorage.setItem(
-          'loggedBlogAppUser', JSON.stringify(user)
-      )
-      blogService.setToken(user.token)
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (exception) {
-      setErrorMessage('Wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
+    const handleLogout = async (event) => {
+        event.preventDefault()
+        window.localStorage.removeItem('loggedBlogAppUser')
+        setUser(null)
     }
-  }
 
-  const loginForm = () => (
-      <>
-        <h1>Notes</h1>
+    const createBlog = async (event) => {
+        event.preventDefault()
+
+        try {
+            const blog = {
+                title,
+                author,
+                url,
+            }
+
+            await blogService.create(blog)
+
+            setTitle('')
+            setAuthor('')
+            setUrl('')
+            blogService.getAll().then((blogs) => setBlogs(blogs))
+        } catch (exception){
+            setErrorMessage(exception.response.data.error)
+            setTimeout(() => {
+                setErrorMessage(null)
+            }, 5000)
+        }
+    }
+
+    const loginForm = () => (<>
+        <h1>blogs</h1>
         <Notification message={errorMessage}/>
 
         <h2>Login</h2>
         <form onSubmit={handleLogin}>
-          <div>
-            username
-            <input
-                type="text"
-                value={username}
-                name="Username"
-                onChange={({target}) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-            <input
-                type="password"
-                value={password}
-                name="Password"
-                onChange={({target}) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
+            <div>
+                username
+                <input
+                    type="text"
+                    value={username}
+                    name="Username"
+                    onChange={({target}) => setUsername(target.value)}
+                />
+            </div>
+            <div>
+                password
+                <input
+                    type="password"
+                    value={password}
+                    name="Password"
+                    onChange={({target}) => setPassword(target.value)}
+                />
+            </div>
+            <button type="submit">login</button>
         </form>
         <Footer/>
-      </>
-  )
+    </>)
 
-  const blogForm = () => (
-     <>{blogs.map(blog =>
-         <Blog key={blog.id} blog={blog} />
-     )}</>
-  )
+    const blogForm = () => (<>{blogs.map(blog => <Blog key={blog.id} blog={blog}/>)}</>)
 
 
-  return (
-    <div>
-      <h2>blogs</h2>
-
-      {user === null ?
-          loginForm() :
-          <div>
+    return (<>
+        {user === null ? loginForm() : <div>
             <p>{user.name} logged-in</p>
+            <button onClick={handleLogout}>logout</button>
             {blogForm()}
-          </div>
-      }
-
-    </div>
-  )
+            <h2>create new</h2>
+            <form onSubmit={createBlog}>
+                <div>
+                    title
+                    <input
+                        value={title}
+                        onChange={({ target }) => setTitle(target.value)}
+                    />
+                </div>
+                <div>
+                    author
+                    <input
+                        value={author}
+                        onChange={({ target }) => setAuthor(target.value)}
+                    />
+                </div>
+                <div>
+                    url
+                    <input value={url} onChange={({ target }) => setUrl(target.value)} />
+                </div>
+                <button type="submit">create</button>
+            </form>
+        </div>}
+    </>)
 }
 
 export default App
